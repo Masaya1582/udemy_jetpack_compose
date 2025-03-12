4615071f567a94b82afcb82b17286f9182d2ff5c
Masaya1582 <masa8y88@gmail.com>
2025-03-12 23:17:17 +0900

    Tip Calculator Refactor前完成

--- MyJetTip/app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ MyJetTip/app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -2,24 +2,32 @@ package com.example.jettipapp
 
 import android.graphics.Paint.Align
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,6 +44,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettipapp.components.InputField
 import com.example.jettipapp.ui.theme.JettipappTheme
+import com.example.jettipapp.util.calculateTotalPerPerson
+import com.example.jettipapp.util.calculateTotalTip
+import com.example.jettipapp.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +73,8 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .height(150.dp)
+            .padding(16.dp)
+            .height(160.dp)
             .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
         color = Color(0xFFE9D7F7)
     ) {
@@ -88,6 +100,16 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 
 @Composable
 fun MainContent() {
+    BillForm() { billAmount ->
+        Log.d("AMT", "MainContent: $billAmount")
+    }
+}
+
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
     val totalBillState = remember {
         mutableStateOf(value = "")
     }
@@ -95,15 +117,34 @@ fun MainContent() {
         totalBillState.value.trim().isNotEmpty()
     }
     val keyboardController = LocalSoftwareKeyboardController.current
+    val sliderPositionState = remember {
+        mutableStateOf(value = 0f)
+    }
+    val splitByState = remember {
+        mutableStateOf(value = 1)
+    }
+    val range = IntRange(start = 1, endInclusive = 100)
+    val tipPercentage = (sliderPositionState.value * 100).toInt()
+    val tipAmountState = remember {
+        mutableStateOf(value = 0.0)
+    }
+    val totalPerPersonState = remember {
+        mutableStateOf(value = 0.0)
+    }
     Surface(
         modifier = Modifier
             .padding(2.dp)
             .fillMaxWidth()
-            .height(240.dp),
+            .height(480.dp),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-        Column {
+        Column(
+            modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            TopHeader(totalPerPerson = totalPerPersonState.value)
             InputField(
                 valueState = totalBillState,
                 labelId = "Enter Bill",
@@ -111,9 +152,90 @@ fun MainContent() {
                 isSingleLine = true,
                 onAction = KeyboardActions {
                     if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
                     keyboardController?.hide()
                 }
             )
+            // if (validState) {
+                Row(
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Split",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = {
+                                splitByState.value =
+                                    if (splitByState.value > 1) splitByState.value -1
+                                else 1
+                            }
+                        )
+                        Text(
+                            text = "${ splitByState.value }",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 9.dp, end = 9.dp)
+                        )
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = {
+                                if (splitByState.value < range.last) {
+                                    splitByState.value = splitByState.value + 1
+                                }
+                            }
+                        )
+                    }
+                }
+//            } else {
+//                Box() {}
+//            }
+            Row(
+                modifier = Modifier.padding(horizontal = 3.dp, vertical = 12.dp)
+            ) {
+                Text(
+                    text = "Tip",
+                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                )
+                Spacer(modifier = Modifier.width(200.dp))
+                Text(
+                    text = "$ ${ tipAmountState.value }",
+                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "$tipPercentage %")
+                Spacer(modifier = Modifier.height(14.dp))
+                Slider(value = sliderPositionState.value, onValueChange = { newVal ->
+                    sliderPositionState.value = newVal
+                    tipAmountState.value = calculateTotalTip(
+                        totalBill = totalBillState.value.toDouble(),
+                        tipPercentage = tipPercentage
+                    )
+                    totalPerPersonState.value =
+                        calculateTotalPerPerson(
+                            totalBill = totalBillState.value.toDouble(),
+                            splitBy = splitByState.value,
+                            tipPercentage = tipPercentage
+                        )
+                },
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                    steps = 5,
+                    onValueChangeFinished = {
+                        Log.d("Finished", "BillForm: ${sliderPositionState.value}")
+                    }
+                    )
+            }
         }
     }
 }
