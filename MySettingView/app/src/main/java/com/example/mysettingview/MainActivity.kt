2b89562c4ececa15ad376d469d7c107dc6bf49b2
Masaya Nakakuki <masa8y88@gmail.com>
2025-03-14 15:11:52 +0900

    Set Font Weight

--- MySettingView/app/src/main/java/com/example/mysettingview/MainActivity.kt
+++ MySettingView/app/src/main/java/com/example/mysettingview/MainActivity.kt
@@ -27,7 +27,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.example.mysettingview.ui.theme.MySettingViewTheme
 
@@ -103,7 +106,8 @@ fun MainProfile(
 			Spacer(modifier = Modifier.width(16.dp))
 			Text(
 				text = name,
-				color = Color.White
+				color = Color.White,
+				fontWeight = FontWeight.Bold,
 			)
 		}
 	}
