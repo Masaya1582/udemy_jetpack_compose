ba9bc307b330db1f77f73dfb1257c45bab331c0c
Masaya Nakakuki <masa8y88@gmail.com>
2025-03-20 10:53:16 +0900

    ViewModelにまとめる

--- MyNoteApp/app/src/main/java/com/example/mynoteapp/MainActivity.kt
+++ MyNoteApp/app/src/main/java/com/example/mynoteapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,9 +16,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mynoteapp.data.NoteDataSource
 import com.example.mynoteapp.model.Note
 import com.example.mynoteapp.screens.NoteScreen
+import com.example.mynoteapp.screens.NoteViewModel
 import com.example.mynoteapp.ui.theme.MyNoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,22 +29,28 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			MyNoteAppTheme {
 				Surface(modifier = Modifier.fillMaxSize()) {
-					val notes = remember {
-						mutableStateListOf<Note>()
-					}
-					NoteScreen(
-						notes = notes,
-						onAddNote = {
-							notes.add(it)
-						},
-						onRemoveNote = {
-							notes.remove(it)
-						}
-					)
+					val noteViewModel: NoteViewModel by viewModels()
+					NotesApp(noteViewModel = noteViewModel)
 				}
 			}
 		}
 	}
+}
+
+@Composable
+private fun NotesApp(
+	noteViewModel: NoteViewModel = viewModel()
+) {
+	val notesList = noteViewModel.getAllNotes()
+	NoteScreen(
+		notes = notesList,
+		onAddNote = {
+			noteViewModel.addNote(it)
+		},
+		onRemoveNote = {
+			noteViewModel.removeNote(it)
+		}
+	)
 }
 
 @Preview(showBackground = true)
