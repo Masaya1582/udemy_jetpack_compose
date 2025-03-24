62354c86bde3269822f220c59771ed2dd50e71d2
Masaya1582 <masa8y88@gmail.com>
2025-03-24 13:46:43 +0900

    interface NoteDatabaseDao

---
+++ MyNoteApp/app/src/main/java/com/example/mynoteapp/data/NoteDatabase.kt
@@ -0,0 +1,1 @@
+package com.example.mynoteapp.dataimport androidx.room.Databaseimport androidx.room.RoomDatabaseimport com.example.mynoteapp.model.Note@Database(entities = [Note::class], version = 1, exportSchema = false)abstract class NoteDatabase: RoomDatabase() {    abstract fun noteDao(): NoteDatabaseDao}
