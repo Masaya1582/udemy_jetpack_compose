97a1a346be8cb49b3628dcb17b90ee02c1a4091f
Masaya Nakakuki <masa8y88@gmail.com>
2025-04-02 11:08:55 +0900

    ブログ用修正

--- MyJSONPractice/app/src/main/java/com/example/myjsonuser/models/User.kt
+++ MyJSONPractice/app/src/main/java/com/example/myjsonuser/models/User.kt
@@ -11,42 +11,16 @@ import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class User(
-    val results: List<ResultsItem>,
-    val info: Info
-)
-
-@Serializable
-data class Timezone(
-    val offset: String,
-    val description: String
-)
-
-@Serializable
-data class Street(
-    val number: Int,
-    val name: String
+    val results: List<ResultsItem>
 )
 
 @Serializable
 data class ResultsItem(
-    val nat: String,
-    val gender: String,
     val phone: String,
-    val dob: Dob,
     val name: Name,
-    val registered: Registered,
     val location: Location,
-    val id: Id,
-    val login: Login,
-    val cell: String,
     val email: String,
     val picture: Picture
-)
-
-@Serializable
-data class Registered(
-    val date: String,
-    val age: Int
 )
 
 @Serializable
@@ -64,65 +38,7 @@ data class Name(
 )
 
 @Serializable
-data class Login(
-    val sha1: String,
-    val password: String,
-    val salt: String,
-    val sha256: String,
-    val uuid: String,
-    val username: String,
-    val md5: String
-)
-
-@Serializable
 data class Location(
     val country: String,
     val city: String,
-    val street: Street,
-    val timezone: Timezone,
-    @Serializable(with = PostcodeSerializer::class) val postcode: String?,
-    val coordinates: Coordinates,
-    val state: String
 )
-
-@Serializable
-data class Info(
-    val seed: String,
-    val page: Int,
-    val results: Int,
-    val version: String
-)
-
-@Serializable
-data class Id(
-    val name: String,
-    val value: String?
-)
-
-@Serializable
-data class Dob(
-    val date: String,
-    val age: Int
-)
-
-@Serializable
-data class Coordinates(
-    val latitude: String,
-    val longitude: String
-)
-
-object PostcodeSerializer : KSerializer<String?> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Postcode", PrimitiveKind.STRING)
-
-    override fun serialize(encoder: Encoder, value: String?) {
-        encoder.encodeString(value ?: "")
-    }
-
-    override fun deserialize(decoder: Decoder): String? {
-        return try {
-            decoder.decodeInt().toString() // Int を String に変換
-        } catch (e: SerializationException) {
-            decoder.decodeString() // 文字列としてデコード
-        }
-    }
-}
