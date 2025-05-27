0e4a090b7b215b26a2f72ee09bfbdbdc977a6476
Masaya1582 <masa8y88@gmail.com>
2025-05-27 14:11:18 +0900

    Navigation遷移をざっくり学ぶ XML

---
+++ MyXMLNavigation/app/src/main/java/com/example/myxmlnavigation/DetailFragment.kt
@@ -0,0 +1,27 @@
+package com.example.myxmlnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myxmlnavigation.databinding.FragmentDetailBinding // ViewBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ MyXMLNavigation/app/src/main/java/com/example/myxmlnavigation/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myxmlnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController // NavControllerを取得するために必要
+import com.example.myxmlnavigation.databinding.FragmentHomeBinding // ViewBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // ボタンがクリックされた時の処理
+        binding.navigateButton.setOnClickListener {
+            // NavControllerを使ってDetailFragmentへ遷移
+            findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
